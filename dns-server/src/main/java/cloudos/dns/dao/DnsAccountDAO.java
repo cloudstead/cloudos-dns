8a69ea48a5b7acfbad4905cff40e0aea12bd37a1
jonathan <jonathan@kyuss.org>
2014-10-04 02:56:53 -0700

    add uniqueHelpers

--- dns-server/src/main/java/cloudos/dns/dao/DnsAccountDAO.java
+++ dns-server/src/main/java/cloudos/dns/dao/DnsAccountDAO.java
@@ -1,8 +1,12 @@
 package cloudos.dns.dao;
 
 import cloudos.dns.model.DnsAccount;
+import org.cobbzilla.util.collection.MapBuilder;
 import org.cobbzilla.wizard.dao.UniquelyNamedEntityDAO;
+import org.cobbzilla.wizard.validation.UniqueValidatorDaoHelper;
 import org.springframework.stereotype.Repository;
+
+import java.util.Map;
 
 @Repository public class DnsAccountDAO extends UniquelyNamedEntityDAO<DnsAccount> {
 
@@ -11,4 +15,12 @@ import org.springframework.stereotype.Repository;
         return found.getPassword().isCorrectPassword(password) ? found : null;
     }
 
+    @Override
+    protected Map<String, UniqueValidatorDaoHelper.Finder<DnsAccount>> getUniqueHelpers() {
+        return MapBuilder.build(new Object[][]{
+                {"name", new UniqueValidatorDaoHelper.Finder<DnsAccount>() {
+                    @Override public DnsAccount find(Object query) { return findByName(query.toString()); }
+                }}
+        });
+    }
 }
