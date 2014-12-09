8094b03003cc25b598efd56904fb4520f632cc1b
jonathan <jonathan@kyuss.org>
2014-12-09 14:42:50 -0800

    fix DbInit

--- dns-server/src/test/java/cloudos/dns/DbInit.java
+++ dns-server/src/test/java/cloudos/dns/DbInit.java
@@ -1,20 +1,28 @@
 package cloudos.dns;
 
+import cloudos.dns.server.DnsServerConfiguration;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+@Slf4j
 public class DbInit extends DnsTestBase {
 
     @Test public void init () throws Exception {
 
         // No longer creating admin below
         // We now create a superuser during chef install, using info in the init data bag
+        final DnsServerConfiguration config = (DnsServerConfiguration) serverHarness.getConfiguration();
+        log.info("initialized database: "+ config.getDatabase().getUrl());
 
         // create cloudstead admin user
 //        final Map<String, String> exports = CommandShell.loadShellExports(".cloudstead.env");
 //        final String user = exports.get("CLOUDOS_DNS_USER");
 //        final String password = exports.get("CLOUDOS_DNS_PASSWORD");
 //        final DnsAccountDAO accountDAO = getBean(DnsAccountDAO.class);
-//        accountDAO.create(new DnsAccount().setName(user).setPassword(new HashedPassword(password)));
+//        final DnsAccount admin = (DnsAccount) new DnsAccount()
+//                .setPassword(new HashedPassword(password))
+//                .setName(user);
+//        accountDAO.create(admin);
     }
 
 }
--- dns-server/src/test/java/cloudos/dns/DnsTestBase.java
+++ dns-server/src/test/java/cloudos/dns/DnsTestBase.java
@@ -2,14 +2,20 @@ package cloudos.dns;
 
 import cloudos.dns.server.DnsServer;
 import cloudos.dns.server.DnsServerConfiguration;
+import lombok.Getter;
+import org.cobbzilla.util.system.CommandShell;
 import org.cobbzilla.wizard.server.config.factory.ConfigurationSource;
 import org.cobbzilla.wizard.server.config.factory.StreamConfigurationSource;
 import org.cobbzilla.wizardtest.resources.ApiDocsResourceIT;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class DnsTestBase extends ApiDocsResourceIT<DnsServerConfiguration, DnsServer> {
+
+    public static final String TEST_ENV_FILE = ".cloudos-dns-test.env";
+    @Getter private final Map<String, String> serverEnvironment = CommandShell.loadShellExportsOrDie(TEST_ENV_FILE);
 
     @Override protected Class<? extends DnsServer> getRestServerClass() { return DnsServer.class; }
 
