e28cc1b4174e0443531bccaebea93c582314cf12
jonathan <jonathan@kyuss.org>
2015-10-06 17:55:58 -0700

    simplify

--- dns-server/src/test/java/cloudos/dns/DnsTestBase.java
+++ dns-server/src/test/java/cloudos/dns/DnsTestBase.java
@@ -3,12 +3,12 @@ package cloudos.dns;
 import cloudos.dns.server.DnsServer;
 import cloudos.dns.server.DnsServerConfiguration;
 import lombok.Getter;
+import org.cobbzilla.util.collection.SingletonList;
 import org.cobbzilla.util.system.CommandShell;
 import org.cobbzilla.wizard.server.config.factory.ConfigurationSource;
 import org.cobbzilla.wizard.server.config.factory.StreamConfigurationSource;
 import org.cobbzilla.wizardtest.resources.ApiDocsResourceIT;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -17,15 +17,10 @@ public class DnsTestBase extends ApiDocsResourceIT<DnsServerConfiguration, DnsSe
     public static final String TEST_ENV_FILE = ".cloudos-dns-test.env";
     @Getter private final Map<String, String> serverEnvironment = CommandShell.loadShellExportsOrDie(TEST_ENV_FILE);
 
-    @Override protected Class<? extends DnsServer> getRestServerClass() { return DnsServer.class; }
-
     protected String getTestConfig() { return "dns-config-test.yml"; }
 
-    @Override
-    protected List<ConfigurationSource> getConfigurations() {
-        final List<ConfigurationSource> sources = new ArrayList<>();
-        sources.add(new StreamConfigurationSource(getClass().getClassLoader().getResourceAsStream(getTestConfig())));
-        return sources;
+    @Override protected List<ConfigurationSource> getConfigurations() {
+        return new SingletonList<ConfigurationSource>(new StreamConfigurationSource(getTestConfig()));
     }
 
 }
